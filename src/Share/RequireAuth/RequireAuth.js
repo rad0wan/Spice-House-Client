1f9bc7c03c0db08bd45acfcaaf810eac4e32dda0
Ridwan <radowan.me@gmail.com>
2022-05-09 22:26:51 +0600

    send email

--- src/Share/RequireAuth/RequireAuth.js
+++ src/Share/RequireAuth/RequireAuth.js
@@ -4,7 +4,7 @@ import auth from '../../firebase.init';
 import Loading from '../../hooks/Loading';
 
 const RequireAuth = ({ children }) => {
-    const [sendEmailVerification, sending, error1] = useSendEmailVerification(auth);
+    // const [sendEmailVerification, sending, error1] = useSendEmailVerification(auth);
 
     let location = useLocation();
 
@@ -18,17 +18,17 @@ const RequireAuth = ({ children }) => {
         return <Navigate to="/login" state={{ from: location }} replace />;
     }
 
-    if (!user.emailVerified) {
-        return <div className='text-center my-5'>
-            <h3 className='text-danger'>You can't verify email.</h3>
-            <h5 className='text-success'>Please verify your email!</h5>
-            <button className='btn btn-success' onClick={async () => {
-                await sendEmailVerification();
-                alert('Sent email');
-            }}
-            >Send Verification</button>
-        </div>
-    }
+    // if (!user.emailVerified) {
+    //     return <div className='text-center my-5'>
+    //         <h3 className='text-danger'>You can't verify email.</h3>
+    //         <h5 className='text-success'>Please verify your email!</h5>
+    //         <button className='btn btn-success' onClick={async () => {
+    //             await sendEmailVerification();
+    //             alert('Sent email');
+    //         }}
+    //         >Send Verification</button>
+    //     </div>
+    // }
 
     return children;
 };
