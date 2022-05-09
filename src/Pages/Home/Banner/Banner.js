c59029f9129e90b14f6bd6e2b2bcc731355a3db5
Ridwan <radowan.me@gmail.com>
2022-05-09 22:55:18 +0600

    cover img

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -17,33 +17,34 @@ const Banner = () => {
                         alt="First slide"
                     />
                     <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        <h3>MAKE YOUR COOKING BLOSSOM</h3>
+                        <p>Essential Spring
+                            Floral Flavors</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="https://cdn.shopify.com/s/files/1/0148/1945/9126/files/Floral_Desktop_x550.jpg?v=1651525801"
+                        src="https://cdn.shopify.com/s/files/1/0148/1945/9126/files/Spices_A-Z_Optimized_1296x.png?v=1573168212"
                         alt="Second slide"
                     />
 
                     <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <h3> Green Pea Falafel</h3>
+                        <p>A unique take on a classic Middle Eastern falafel! </p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="https://cdn.shopify.com/s/files/1/0148/1945/9126/files/Floral_Desktop_x550.jpg?v=1651525801"
+                        src="https://cdn.shopify.com/s/files/1/0564/3904/6331/files/spring-savings_banner_1200x560_crop_center.jpg?v=1651347794"
                         alt="Third slide"
                     />
 
                     <Carousel.Caption>
-                        <h3>Third slide label</h3>
+                        <h3>Cooking made simple</h3>
                         <p>
-                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                            With Spice 'n Easy, cooking is as simple as 1, 2, 3!
                         </p>
                     </Carousel.Caption>
                 </Carousel.Item>
